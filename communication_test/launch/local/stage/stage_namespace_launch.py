fc09db4dceaa854bf5816b15347dd25dad8149d3
Lucas NAURY <lucas.naury@sfr.fr>
2024-05-22 16:28:48 +0200

    Test namespace launch

--- communication_test/launch/local/stage/stage_namespace_launch.py
+++ communication_test/launch/local/stage/stage_namespace_launch.py
@@ -22,28 +22,27 @@ def generate_launch_description():
                 'launch/stage.launch.py')),
         launch_arguments={
             'world':'cave_three_robots',
+            'enforce_prefixes':'true',
             'one_tf_tree':'false'
         }.items()
     )
 
-    # Create turtles with their own namespace 'robotX'
+    # Create 3 turtles with their own namespace 'robotX'
     turtles = []
     for i in range(3):
         turtles.append(
-            GroupAction([
-                PushRosNamespace(f'robot_{i}'),
-                IncludeLaunchDescription(
-                    PythonLaunchDescriptionSource(
-                        os.path.join(
-                            get_package_share_directory('communication_test'),
-                            'launch/include/stage/stage_namespace_robot_launch.py')),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    os.path.join(
+                        get_package_share_directory('communication_test'),
+                        'launch/include/stage/stage_namespace_robot_launch.py')),
 
-                    # Launch turtles with the correct DDS configuration
-                    launch_arguments={
-                        'robot_id': str(i)
-                    }.items()
-                )
-            ])
+                # Launch turtles with the correct DDS configuration
+                launch_arguments={
+                    'namespace': f"robot_{i}",
+                    'robot_id': str(i)
+                }.items()
+            )
         )
 
     # Launch operator node only in the common network
@@ -63,19 +62,22 @@ def generate_launch_description():
                 get_package_share_directory('communication_test'),
                 'launch/include/rviz_launch.py')),
         launch_arguments={
-            "config": os.path.join(get_package_share_directory('communication_test'), 'config', 'stage.rviz')
+            "config": os.path.join('config', 'stage.rviz')
         }.items()
     )
 
-    load_map = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(
-                get_package_share_directory('communication_test'),
-                'launch/include/load_map.py')),
-        launch_arguments={
-            "map_file": os.path.join(get_package_share_directory('communication_test'), 'config', 'maps', 'cave', 'map.yaml')
-        }.items()
+    init_pose_node = Node(
+        package='communication_test',
+        executable='stage_init_pose.py',
+        name='init_pose',
+        parameters=[
+            {
+                'nb_robots': 3,
+                'wait': 15
+            }
+        ]
     )
+
 
     return LaunchDescription([
         # Rviz with specific config
@@ -90,7 +92,7 @@ def generate_launch_description():
         # Run operator node
         operator_node,
 
-        # Load map
-        load_map,
+        # Node to initialize all robot poses at startup for AMCL
+        init_pose_node
         
     ])
