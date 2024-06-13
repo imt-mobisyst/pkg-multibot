602ea71d6aa73fb13368320dc30d733db455e1d4
Lucas NAURY <lucas.naury@sfr.fr>
2024-06-13 14:23:58 +0200

    Test

---
+++ communication_test/launch/pibot/warehouse_demo/warehouse_namespace_launch.py
@@ -0,0 +1,82 @@
+import os, socket
+from ament_index_python import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, GroupAction
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import TextSubstitution
+from launch_ros.actions import Node, PushRosNamespace
+from launch.actions import IncludeLaunchDescription, OpaqueFunction, SetLaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+def generate_launch_description():
+
+    # CLI arguments
+    log_level_launch_arg = DeclareLaunchArgument(
+        'nav_log_level', default_value='info', description='log level'
+    )
+
+    robot_id = int(socket.gethostname()[-2:])
+    namespace = f"robot_{robot_id}"
+
+    # Start a controller node
+    controller_node = GroupAction([
+        PushRosNamespace(namespace),
+        Node(
+            package='communication_test',
+            executable='stage_controller.py',
+            name='stage_controller',
+            parameters=[
+                {
+                    'robot_id': robot_id,
+                }
+            ]
+        )
+    ])
+    
+
+    localization = GroupAction([
+        PushRosNamespace(namespace),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                        os.path.join(
+                            get_package_share_directory('nav2_bringup'),
+                            'launch/localization_launch.py')),
+            launch_arguments={
+                "namespace": namespace,
+                'map': os.path.join(get_package_share_directory('communication_test'), 'map', 'map.yaml'),
+                'params_file': os.path.join(get_package_share_directory('communication_test'), 'config', 'nav2', 'nav2_params_kobuki.yaml'),
+                'autostart': 'True',
+                'use_sim_time': 'True',
+                'log_level': LaunchConfiguration('nav_log_level')
+            }.items()
+        )
+    ])
+
+
+
+    nav2 = GroupAction([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(
+                    get_package_share_directory('communication_test'),
+                    'launch/nav/navigation_launch.py')),
+            launch_arguments={
+                "namespace": namespace,
+                "params_file": os.path.join(get_package_share_directory('communication_test'), 'config', 'nav2', 'nav2_params_kobuki.yaml'),
+                'log_level': LaunchConfiguration('nav_log_level')
+            }.items()
+        )
+    ])
+    
+  
+
+    return LaunchDescription([
+        log_level_launch_arg,
+
+        controller_node,
+
+        localization,
+
+        nav2
+    ])
