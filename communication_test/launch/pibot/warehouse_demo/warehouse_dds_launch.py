6b41956b367bcae35dd4ec6554a5311ebffe8cdc
Lucas NAURY <lucas.naury@sfr.fr>
2024-06-14 09:48:54 +0200

    Fix DDS Discovery servers launch files

--- communication_test/launch/pibot/warehouse_demo/warehouse_dds_launch.py
+++ communication_test/launch/pibot/warehouse_demo/warehouse_dds_launch.py
@@ -14,10 +14,11 @@ def generate_launch_description():
         'nav_log_level', default_value='info', description='log level'
     )
 
-    # CLI arguments
-    local_dds_server = "127.0.0.1:11811"
-    subnet_dds_server_launch_arg = DeclareLaunchArgument("subnet_dds_server")
+    this_ip = socket.gethostbyname(socket.gethostname())
 
+    # CLI arguments
+    subnet_dds_server_launch_arg = DeclareLaunchArgument("subnet_dds_server")
+    robot_ip_launch_arg = DeclareLaunchArgument('robot_ip',default_value=this_ip)
 
     # Robot ID
     robot_id = int(socket.gethostname()[-2:])
@@ -28,10 +29,13 @@ def generate_launch_description():
 
     def create_common_servers(context):
         subnet_dds_server =  LaunchConfiguration('subnet_dds_server').perform(context)
+        robot_ip = LaunchConfiguration('robot_ip_launch_arg').perform(context)
+
+        robot_dds_server = robot_ip + ":11811"
 
         # The ROS_DISCOVERY_SERVER variable must list the servers as a list with their ID as index
         # When running on the same machine, we need to add multiple ";" to match the many IDs
-        controller_servers = subnet_dds_server + (";"*robot_id) + local_dds_server
+        controller_servers = subnet_dds_server + (";"*robot_id) + (robot_dds_server)
 
         return [SetLaunchConfiguration('common_servers', controller_servers)]
 
@@ -39,14 +43,17 @@ def generate_launch_description():
 
 
     def create_local_servers(context):
+        robot_ip = LaunchConfiguration('robot_ip_launch_arg').perform(context)
+
+        robot_dds_server = robot_ip + ":11811"
+
         # The ROS_DISCOVERY_SERVER variable must list the servers as a list with their ID as index
         # When running on the same machine, we need to add multiple ";" to match the many IDs
-        controller_servers = (";"*(robot_id + 1)) + local_dds_server
+        controller_servers = (";"*(robot_id + 1)) + robot_dds_server
 
         return [SetLaunchConfiguration('local_servers', controller_servers)]
 
     local_servers_arg = OpaqueFunction(function=create_local_servers)
-
 
 
     # START NODES
