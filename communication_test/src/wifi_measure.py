f81dbce3de46b30cf1fa0a1e26fc10f9ef0bf402
Lucas NAURY <lucas.naury@sfr.fr>
2024-06-21 14:57:11 +0200

    Add Wifi measure node with service

---
+++ communication_test/src/wifi_measure.py
@@ -0,0 +1,128 @@
+#!/usr/bin/python3
+import rclpy
+from rclpy.node import Node
+import subprocess
+
+from communication_test_interfaces.srv import WifiMeasure
+
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Point
+
+class WifiMeasureNode(Node):
+
+    def __init__(self):
+        super().__init__('wifi_measure')
+
+
+        self.declare_parameter('interface', "wlp2s0")
+
+        # Create service server
+        self.srv = self.create_service(WifiMeasure, 'wifi_measure', self.serviceHandler)
+
+        # Create publisher
+        self.measurePublisher = self.create_publisher(Marker, 'wifi_measure', 10)
+
+        # Store values
+        self.nbMeasures = 0
+
+
+
+
+    def serviceHandler(self, request:WifiMeasure.Request, response:WifiMeasure.Response):
+        # Take measure
+        measure = self.takeMeasure()
+
+        if measure:
+            # Split measure
+            quality, dBm = measure
+
+            # Create response
+            response.signal_quality = float(quality)
+            response.signal_dbm     = float(dBm)
+
+            # Publish measure
+            if request.publish:
+                self.publishMeasure(request.position, quality)
+
+        
+        return response
+
+
+    def takeMeasure(self):
+        # Get all wifi measures
+        proc = subprocess.Popen(["iwlist", self.interface(), "scan"],stdout=subprocess.PIPE, universal_newlines=True)
+        out, err = proc.communicate()
+
+        # Get the line that interests us
+        lines = out.split("\n")
+        line = None
+        for l in lines:
+            if "Quality=" in l:
+                line = l.strip()
+                break
+
+
+        # The line is in the form : "Quality=47/70  Signal level=-63 dBm"
+        if line is not None:
+            self.nbMeasures += 1
+
+            # Get signal quality
+            quality = line.split('=')[1].split(" ")[0].split('/')
+            quality = float(quality[0]) / float(quality[1]) * 100
+
+            # Get signal strength in dBm
+            dBm = int(line.split('=')[2].split(" ")[0])
+
+
+            self.get_logger().debug(f"Quality : {quality}\t Strength : {dBm} dBm")
+
+
+            return quality, dBm
+
+
+
+
+
+    def publishMeasure(self, position:Point, quality:float):
+        marker = Marker()
+        marker.header.frame_id = 'map'
+        marker.header.stamp = self.get_clock().now().to_msg()
+
+        # Set shape
+        marker.type = Marker.CYLINDER
+        marker.id = self.nbMeasures
+
+        # Set the scale of the marker
+        marker.scale.x = 0.15
+        marker.scale.y = 0.15
+        marker.scale.z = 0.01
+
+        # Set the color
+        marker.color.r = 1.0 - quality
+        marker.color.g = quality
+        marker.color.b = 0.0
+        marker.color.a = 1.0
+
+        # Set the pose of the marker
+        marker.pose.position = position
+
+        # Publish marker
+        self.measurePublisher.publish(marker)
+
+
+    def interface(self):
+        return self.get_parameter('interface').get_parameter_value().string_value
+
+
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = WifiMeasureNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
