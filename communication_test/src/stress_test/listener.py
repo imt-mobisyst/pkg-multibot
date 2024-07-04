de08851579377f5ac1e213a631e0182109a4eac4
Lucas NAURY <lucas.naury@sfr.fr>
2024-07-04 10:22:09 +0200

    Stress test nodes

---
+++ communication_test/src/stress_test/listener.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+import rclpy
+from rclpy.node import Node
+
+import rclpy.time
+from std_msgs.msg import String
+
+class ListenerNode(Node):
+
+    def __init__(self):
+        super().__init__('listener')
+
+        # Init subscribers
+        self.publisher = self.create_subscription(String, '/test', self.receiveMessage, 10)
+
+        self.get_logger().info('Listening...')
+    
+
+        self.totalNbMessages = 0
+    
+    
+
+    def receiveMessage(self, msg):
+        """
+        Receive a message to another computer
+        """
+
+        if self.totalNbMessages == 0:
+            self.initTime = self.get_clock().now()
+
+
+
+        # Check current time
+        current_time = self.get_clock().now()
+        duration = (current_time - self.initTime).nanoseconds * 10**(-9)
+
+        
+        # Save data
+        self.totalNbMessages += 1
+        self.get_logger().info(f"Received message n°{self.totalNbMessages} at time {duration}")
+            
+            
+
+
+
+
+
+
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = ListenerNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ communication_test/src/stress_test/talker.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+import rclpy
+from rclpy.node import Node
+
+import rclpy.time
+from std_msgs.msg import String
+
+class TalkerNode(Node):
+
+    def __init__(self):
+        super().__init__('talker')
+
+
+        # Declare ROS parameters
+        self.declare_parameter('duration', 10.0)
+        self.declare_parameter('frequency', 100.0)
+
+        # Init publishers
+        self.publisher = self.create_publisher(String, '/test', 10)
+        
+        # Init loop
+        self.create_timer(1.0 / self.paramDouble('frequency'), self.sendMessage)
+
+        self.totalNbMessages = 0
+
+        self.initTime = self.get_clock().now()
+    
+    def paramDouble(self, name):
+        return self.get_parameter(name).get_parameter_value().double_value
+    
+
+    def sendMessage(self):
+        """
+        Send a message to another computer
+        """
+
+        # Check current time
+        current_time = self.get_clock().now()
+        duration = (current_time - self.initTime).nanoseconds * 10**(-9)
+
+        
+        if duration > self.paramDouble('duration'):
+            self.get_logger().info(f"-> Successfully sent {self.totalNbMessages} messages")
+            self.destroy_node()
+            exit()
+            
+
+        # Send message
+        str = String()
+        str.data = "Hello world"
+
+        self.publisher.publish(str)
+
+        self.totalNbMessages += 1
+        self.get_logger().info(f"Sending message n°{self.totalNbMessages} at time {duration}")
+
+
+
+
+
+
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = TalkerNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
